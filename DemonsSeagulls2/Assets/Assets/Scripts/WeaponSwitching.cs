9ff31d37a35cad2f599c4c68ebc94a552d00e27f
Elliot Lambert <elliotlambert1@yahoo.com>
2021-03-08 18:37:03 +0000

    New Repository, Project rebuild from scratch. Everything working as it was before.

---
+++ DemonsSeagulls2/Assets/Assets/Scripts/WeaponSwitching.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitching : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Switch1"))
+        {
+            Debug.Log("Switched to Manual");
+            GetComponent<ShootingRifle>().enabled = true;
+            GetComponent<ShootingAuto>().enabled = false;
+            GetComponent<ShootingMelee>().enabled = false;
+        }
+        if (Input.GetButtonDown("Switch2"))
+        {
+            Debug.Log("Switched to Auto");
+            GetComponent<ShootingRifle>().enabled = false;
+            GetComponent<ShootingAuto>().enabled = true;
+            GetComponent<ShootingMelee>().enabled = false;
+        }
+        if (Input.GetButtonDown("Switch3"))
+        {
+            Debug.Log("Switched to Melee");
+            GetComponent<ShootingRifle>().enabled = false;
+            GetComponent<ShootingAuto>().enabled = false;
+            GetComponent<ShootingMelee>().enabled = true;
+        }
+    }
+}
